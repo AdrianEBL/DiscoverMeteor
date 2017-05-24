e667b1c018f9b7c3592ed019014e82c8d1892147
JuanChacon <virgo_gt@hotmail.com>
2017-05-23 18:18:12 -0600

    Publicando propiedades parciales

--- meteorapp/server/publications.js
+++ meteorapp/server/publications.js
@@ -1,4 +1,21 @@
 // on the server
-Meteor.publish('posts', function(author) {
-  return Posts.find({flagged: false, author: author});
+//Meteor.publish('posts', function(author) {
+  //return Posts.find({flagged: false, author: author});
+//});
+
+//Meteor.publish('somePosts', function(){
+  //return Posts.find({'author':'Tom'});
+//});
+
+//Meteor.publish('allPosts', function(){
+  //return Posts.find({}, {fields: {
+    //date: false
+  //}});
+//});
+
+
+Meteor.publish('allPosts', function(){
+  return Posts.find({'author':'Tom'}, {fields: {
+    date: false
+  }});
 });
