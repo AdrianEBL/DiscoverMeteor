b442f75a46d308c6bf4477bbc678a228f4bb1982
AdrianEBL <ed.aprio@gmail.com>
2017-05-27 00:58:55 -0600

    Ampliacion del metodo que crea los comentarios para que llame a la nueva funcion creada

--- meteorapp/lib/collections/comments.js
+++ meteorapp/lib/collections/comments.js
@@ -2,11 +2,13 @@ Comments = new Mongo.Collection('comments');
 
 Meteor.methods({
   commentInsert: function(commentAttributes) {
+    
     check(this.userId, String);
     check(commentAttributes, {
       postId: String,
       body: String
     });
+    
     var user = Meteor.user();
     var post = Posts.findOne(commentAttributes.postId);
     if (!post)
@@ -17,11 +19,12 @@ Meteor.methods({
       author: user.username,
       submitted: new Date()
     });
-      
-      
-      // update the post with the number of comments
-      Posts.update(comment.postId, {$inc: {commentsCount: 1}});
- 
-      return Comments.insert(comment);
+   
+    Posts.update(comment.postId, {$inc: {commentsCount: 1}});
+
+    comment._id = Comments.insert(comment);
+    
+    createCommentNotification(comment);
+    return comment._id;
   }
 });
