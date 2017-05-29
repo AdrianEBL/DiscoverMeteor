d98c1ac7af9a34fe8106c1f0cf13655e9b6c04b8
AdrianEBL <ed.aprio@gmail.com>
2017-05-28 23:58:26 -0600

    commit

--- meteorapp/server/publications.js
+++ meteorapp/server/publications.js
@@ -1,45 +1,22 @@
-/*
-// on the server
-//Meteor.publish('posts', function(author) {
-  //return Posts.find({flagged: false, author: author});
-//});
-
-//Meteor.publish('somePosts', function(){
-  //return Posts.find({'author':'Tom'});
-//});
-
-//Meteor.publish('allPosts', function(){
-  //return Posts.find({}, {fields: {
-    //date: false
-  //}});
-//});
-
-
-//Meteor.publish('allPosts', function(){
-  //return Posts.find({'author':'Tom'}, {fields: {
-    //date: false
-  //}});
-//});
-*/
 Meteor.publish('posts', function(options) {
-   check(options, {
-     sort: Object,
-     limit: Number
-   });
-   return Posts.find({}, options);
- });
- 
+  check(options, {
+    sort: Object,
+    limit: Number
+  });
+  return Posts.find({}, options);
+});
+
 Meteor.publish('singlePost', function(id) {
   check(id, String);
   return Posts.find(id);
 });
 
 
- Meteor.publish('comments', function(postId) {
-   check(postId, String);
-   return Comments.find({postId: postId});
- });
- 
- Meteor.publish('notifications', function() {
-   return Notifications.find({userId: this.userId, read: false});
- });
+Meteor.publish('comments', function(postId) {
+  check(postId, String);
+  return Comments.find({postId: postId});
+});
+
+Meteor.publish('notifications', function() {
+  return Notifications.find({userId: this.userId, read: false});
+});
